4412b02819d189320c8d0eae8f478ace50c11a8b
Virginia Amb <virginiaamb@gmail.com>
2023-01-19 09:29:53 -0300

    modifying a class

--- poo/poo.py
+++ poo/poo.py
@@ -7,25 +7,54 @@ All classes should have them. It is called automatically when a new object is cr
 '''Self parameter is a reference to the current instance of the class. It is the first parameter of any method (function) in
 a class. It is used to access variables and methods belonging to the class. Also used to add attributes to a method.'''
 
+# class Instructors:
+#     companyName = 'Bluelime'
+
+#     def __init__(self, course):
+#         self.course = course
+
+#     def printinfo(self):
+#         print("My Company name is:", Instructors.companyName)
+
+'''instantiating a class: process of creating an object from a class
+creamos una variable y la igualamos a la clase.'''
+
+# elearning = Instructors("Python for beginners") # dentro del parentesis pasamos un valor o argumento para el parametro.
+'''ese valor va a reemplazar el valor inicial 'course' '''
+
+# bls = Instructors("Django for beginners")
+
+'''in order to call the method on an instance of the class: '''
+# bls.printinfo()
+
+'''access attributes from an instance that has been created from a class'''
+# print(bls.course)
+
+'''Modifying a class
+- add new attributes'''
+
 class Instructors:
     companyName = 'Bluelime'
 
-    def __init__(self, course):
+    def __init__(self, course, duration):
         self.course = course
+        self.duration = duration
 
     def printinfo(self):
         print("My Company name is:", Instructors.companyName)
 
-# instantiating a class: process of creating an object from a class
-# creamos una variable y la igualamos a la clase.
+bls = Instructors("Django for beginners", 8)
 
-elearning = Instructors("Python for beginners") # dentro del parentesis pasamos un valor o argumento para el parametro.
-# ese valor va a reemplazar el valor inicial 'course'
+'''modify existing attribute values'''
+bls.course = "HTML"
 
-bls = Instructors("Django for beginners")
-
-# in order to call the method on an instance of the class:
 bls.printinfo()
 
-# access attributes from an instance that has been created from a class
 print(bls.course)
+
+'''delete attributes'''
+del bls.duration
+
+# print(bls.duration)
+
+'''modify existing methods'''
