3d26df321ea34a0198db1a88f162bc1015e6b750
Virginia Amb <virginiaamb@gmail.com>
2023-01-20 11:54:07 -0300

    Update poo.py

--- poo/poo.py
+++ poo/poo.py
@@ -7,28 +7,28 @@ All classes should have them. It is called automatically when a new object is cr
 '''Self parameter is a reference to the current instance of the class. It is the first parameter of any method (function) in
 a class. It is used to access variables and methods belonging to the class. Also used to add attributes to a method.'''
 
-# class Instructors:
-#     companyName = 'Bluelime'
+class Instructors:
+    companyName = 'Bluelime'
 
-#     def __init__(self, course):
-#         self.course = course
+    def __init__(self, course):
+        self.course = course
 
-#     def printinfo(self):
-#         print("My Company name is:", Instructors.companyName)
+    def printinfo(self):
+        print("My Company name is:", Instructors.companyName)
 
 '''instantiating a class: process of creating an object from a class
 creamos una variable y la igualamos a la clase.'''
 
-# elearning = Instructors("Python for beginners") # dentro del parentesis pasamos un valor o argumento para el parametro.
+elearning = Instructors("Python for beginners") # dentro del parentesis pasamos un valor o argumento para el parametro.
 '''ese valor va a reemplazar el valor inicial 'course' '''
 
-# bls = Instructors("Django for beginners")
+bls = Instructors("Django for beginners")
 
 '''in order to call the method on an instance of the class: '''
-# bls.printinfo()
+bls.printinfo()
 
 '''access attributes from an instance that has been created from a class'''
-# print(bls.course)
+print(bls.course)
 
 '''Modifying a class
 - add new attributes'''
